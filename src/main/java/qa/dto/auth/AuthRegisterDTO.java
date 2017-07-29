762bfa3853ef55fb1ca4bf4f554f60300327bf07
Ozgur GUL <lugrugzo@gmail.com>
2017-07-30 00:58:30 +0300

    displayName will be necessary when register

--- src/main/java/qa/dto/auth/AuthRegisterDTO.java
+++ src/main/java/qa/dto/auth/AuthRegisterDTO.java
@@ -1,8 +1,15 @@
 package qa.dto.auth;
 
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
+
 /**
  * Created by ozgur on 7/8/17.
  */
 public class AuthRegisterDTO extends AuthLoginDTO {
+
+    // @NotBlank(message = "İsim boş olamaz")
+    // @Length(max = 200)
+    // public String displayName;
 
 }
